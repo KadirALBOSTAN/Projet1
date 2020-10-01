56b404b7a92cf5bb391f6e39c7e37e8857b4e4b1
Kadir <kadir.albostan@gmail.com>
2020-10-01 14:48:01 +0200

    Page Formation

---
+++ formations.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8" />
+    <title>CV de Jeff de Tuche</title>
+    <link rel="stylesheet" href="formations.css">
+</head>
+
+<body>
+<section>
+
+    <h1>Formations</h1>
+
+    <div class="exp-bac">
+        <h3>- BAC PRO commerce spécialité Frites</h3>
+        <p><br>2007-2010<br>
+            Non obtenu<br>
+        Lycée Sergio Garcia Zorro, Bouzolles</p>
+    </div>
+
+    <div class="exp-brevet">
+        <h3>- Brevet des collèges</h3>
+        <p>2003-2007<br>
+            Non obtenu<br>
+            Collège Alain Delon, Bouzolles</p>
+    </div>
+
+    <div class="exp">
+        <h3>- Chômeur professionnel</h3>
+        <p>Bouzolles</p>
+    </div>
+
+        <h2>Compétences</h2>
+
+        <div class="comp">
+            <p>Jeux d'argent</p>
+            <div class="conteneur-barre"><span class="barre c100"></span></div>
+        </div>
+
+        <div class="comp">
+            <p>Accent nordiste</p>
+            <div class="conteneur-barre"><span class="barre c95"></span></div>
+        </div>
+
+        <div class="comp">
+            <p>Chirurgien esthétique sans diplôme</p>
+            <div class="conteneur-barre"><span class="barre c90"></span></div>
+        </div>
+    </section>
+</body>
+</html>
