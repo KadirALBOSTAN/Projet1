afb6982c0b0aa14f45c9c523cf6c87b991bb3fbf
Becker <mathieubecker86@gmail.com>
2020-10-01 14:54:41 +0200

    Page Expérience

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+    <nav>
+        <ul class="navbar">
+            <li class="list"><a href="index.php">HOME</a></li>
+            <li class="list"><a href="quisuis-je.php">QUI SUIS-JE?</a></li>
+            <li class="list"><a href="bikes">FORMATION</a></li>
+            <li class="list"><a href="bikes">EXPÉRIENCES PROFESSIONNELLES</a></li>
+            <li class="list"><a href="bikes">CITATIONS</a></li>
+            <li class="list"><a href="bikes">CONTACT</a></li>
+        </ul>
+    </nav>
+</header>
