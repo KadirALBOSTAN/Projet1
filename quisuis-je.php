ac832909614e08f2f94651e1430d8c1f99d7065c
Erwan Cloitre <erwan.cloitre@gmail.com>
2020-10-01 14:03:04 +0200

    Init

---
+++ quisuis-je.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Jeff Tuche</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+<section class="secondpage">
+    <?php include('header.php'); ?>
+    <main class="colonne">
+        <img class="logo" src="jeff.png">
+        <h1 class="title">Qui suis-je?</h1>
+        <p class="descri">Et moi, c'est Tuche, avec un T.
+            Comme T'es là. T'es là, t'es là ou t'es pas là ?
+            T'es pas là ? Ben où c'est qu't'es ? Où c'est qu't'es ? T'es pas là ?
+            T'es pas là ? Où c'est qu't'es ? Ah, t'es là !
+            T'es là ou t'es pas là avec un T, comme Tuche, comme T'es là. Tuche, t'es là
+            </p>
+        <h1 class="title">Contact Details</h1>
+        <p class="descri">Jeff Tuche</p>
+        <p class="descri">1600 Amphitheatre Parkway</p>
+        <p class="descri">Mountain View, CA 94043 US</p>
+        <p class="descri">(123)456-7890</p>
+    </main>
+</section>
+<footer>
+
+</footer>
+</body>
+</html>
