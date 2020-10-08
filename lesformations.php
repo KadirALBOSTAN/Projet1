293b00b36eefc7a7bd822be70888c97da76db8a3
Kadir <kadir.albostan@gmail.com>
2020-10-08 12:23:57 +0200

    envoie des fichiers

---
+++ lesformations.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="fr" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CV de Jeff de Tuche</title>
+    <link rel="stylesheet" href="lesformations.css">
+    <!-- NAVBAR RESPONSIVE & FOOTER-->
+    <link rel="stylesheet" media="screen and (min-width: 1025px)" href="navbar_footer_CSS/header.css">
+    <link rel="stylesheet" media="screen and (max-width: 1024px)" href="navbar_footer_CSS/burgerMenu.css">
+    <link rel="stylesheet" href="navbar_footer_CSS/footer.css">
+
+</head>
+
+<body>
+<?php include('header.php'); ?>
+<?php include('footer.php'); ?>
+
+<section>
+
+    <h1>FORMATIONS</h1>
+    <div class="exp">
+        <h3>Chômeur</h3>
+            <p class="date">Depuis 2010</p><br>
+            <p>Depuis que je suis inscrit à Pôle Emploi, je vis ma meilleure vie. Je profite de mon temps libre pour ne rien faire. Et surtout faire chier Monnier.</p
+    </div>
+
+    <div class="exp-bac">
+        <h3>BAC PRO commerce spécialité Frites</h3>
+            <p class="date">2004-2007</p><br>
+            <p>Non obtenu<br>
+            Lycée Sergio Garcia Zorro, Bouzolles<br>
+            Après le brevet, je me lance dans l'obtention du baccalauréat qui se termine sans succès.</p>
+    </div>
+
+    <div class="exp-brevet">
+        <h3>Brevet des collèges</h3>
+            <p class="date">2000-2004</p><br>
+            <p>Non obtenu<br>
+            Collège Alain Delon, Bouzolles</p>
+    </div>
+
+        <h1>COMPETENCES</h1>
+
+        <div class="comp">
+            <p>Jeux d'argent</p>
+            <div class="progress center-block">
+                <progress id="avancement" class="progressBar" value="100" max="100"></progress>
+                <div class="progressValue">100%</div>
+            </div>
+        </div>
+
+        <div class="comp">
+            <p>Accent nordiste</p>
+            <div class="progress">
+                <progress id="avancement" class="progressBar" value="100" max="100"></progress>
+                <div class="progressValue">100%</div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
